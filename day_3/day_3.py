3eff020eb508ffb772438f939075a9eeb498a414
Jeremias Shadbolt <jeremias.r.shadbolt@utu.fi>
2022-12-03 16:42:03 +0200

    Refactored the calculation of scores

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,4 +1,4 @@
-
+import string
 #### PART 1 helper functions ####
 # divides the items in the rucksack into two separate compartments
 def divide_items(item: str)-> tuple:
@@ -20,35 +20,32 @@ def divide_to_groups(list_of_elves: list) -> list:
 
     return groups
 
+# get score of item
+def score(char: str) -> int:
+    return string.ascii_letters.index(char) + 1
+
+# get mutual item in group of elves
 def find_mutual_in_group(group: list)-> str:
     return ''.join(set(group[0]).intersection(group[1]).intersection(group[2]))
 
 def solution():
-    import string
     with open('input.txt', 'r') as f:
         input = f.read().split('\n')
 
-    priorities_lower = dict(zip(string.ascii_lowercase, [priority for priority in range(1, 27)]))
-    priorities_upper = dict(zip(string.ascii_uppercase, [priority for priority in range(27, 53)]))
-
+    
     priority_sum_part_1 = 0
     for item in input:
         mutual_item = get_same_item(item)
-        if mutual_item.islower():
-            priority_sum_part_1 += priorities_lower[mutual_item]
-        else:
-            priority_sum_part_1 += priorities_upper[mutual_item]
+        priority_sum_part_1 += score(mutual_item)
 
     priority_sum_part_2 = 0
     for group in divide_to_groups(input):
         mutual_item_in_group = find_mutual_in_group(group)
-        if mutual_item_in_group.islower():
-            priority_sum_part_2 += priorities_lower[mutual_item_in_group]
-        else:
-            priority_sum_part_2 += priorities_upper[mutual_item_in_group]
-    
+        priority_sum_part_2 += score(mutual_item_in_group)
+
     print(f"Part 1 solution: {priority_sum_part_1}")
     print(f"Part 2 solution: {priority_sum_part_2}")
+
 
 solution()
 
