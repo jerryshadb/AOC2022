d18a2abab275bd9ca7f80c2ac587eb3699704eb2
Jeremias Shadbolt <jeremias.r.shadbolt@utu.fi>
2022-12-05 18:33:09 +0200

    added comments explaining thought process

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -4,19 +4,25 @@ def solution():
 
     assignments_overlap = 0
     pair_overlap = 0
+    # go through assignments in input
     for assignment in input:
+        # split the assignments into two, so 1 per elf and give each elf a starting and ending point (so their sections)
         assignment_1, assignment_2 = assignment.split(',')
         elf_1_start, elf_1_end = [int(r) for r in assignment_1.split('-')]
         elf_2_start, elf_2_end = [int(r) for r in assignment_2.split('-')]
 
+        # check whether either one of them fully contain the other by comparing starting and ending values
         if (elf_1_start <= elf_2_start and elf_1_end >= elf_2_end) or (elf_2_start <= elf_1_start and elf_2_end >= elf_1_end):
             assignments_overlap += 1
         
+        # create a list of starting and ending points
         range_1 = list(range(elf_1_start, elf_1_end + 1))
         range_2 = list(range(elf_2_start, elf_2_end + 1))
-        
+
+        # check if sections of the 1st elf are in the sections of the 2nd elf (so mutual overlap anywhere in assignments)
         overlap = [i for i in range_1 if i in range_2]
 
+        # if there were any, there was some
         if len(overlap) > 0:
             pair_overlap += 1
 
